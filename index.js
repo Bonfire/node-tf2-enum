a3bbeefaa5a4b669508dc8af95dccdf3ae55baa5
Bonfire <baranb@knights.ucf.edu>
2021-01-25 22:09:47 -0500

    fix: Add prettierrc, Add consistent quoting

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 var QUALITY = require("./lib/quality.js");
 var CRAFTWEAPON = require("./lib/craftWeapon.js");
 
-module.exports = [QUALITY, CRAFTWEAPON];
+exports.QUALITY = QUALITY;
+exports.CRAFTWEAPON = CRAFTWEAPON;
