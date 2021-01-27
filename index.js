4f3159dae42098a0e272f72254916526ca70f9a5
Bonfire <baranb@knights.ucf.edu>
2021-01-26 23:44:52 -0500

    feat: add strange part

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const QUALITY = require("./lib/quality.js");
 const CRAFTWEAPON = require("./lib/craftweapon.js");
 const STOCKWEAPON = require("./lib/stockweapon.js");
 const NOISEMAKER = require("./lib/noisemaker.js");
+const STRANGEPART = require("./lib/strangepart.js");
 
 exports.QUALITY = QUALITY;
 exports.CRAFTWEAPON = CRAFTWEAPON;
 exports.STOCKWEAPON = STOCKWEAPON;
 exports.NOISEMAKER = NOISEMAKER;
+exports.STRANGEPART = STRANGEPART;
